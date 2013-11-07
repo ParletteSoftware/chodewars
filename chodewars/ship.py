db6b24a3a9a69c075f6ef668388e03c10280b8e7
Matt Parlette <matthew.parlette@gmail.com>
2013-11-07 14:58:29 -0500

    Starting Clean.

--- chodewars/ship.py
+++
@@ -1,8 +0,0 @@
-from entity import Entity
-from sector import Sector
-
-class Ship(Entity):
-  def __init__(self,initial_state = {}):
-    super(Ship,self).__init__(initial_state = initial_state)
-    
-    self.holds = initial_state['holds'] if 'holds' in initial_state else 10
