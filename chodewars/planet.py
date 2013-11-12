f1f9663bf7fc3db1a6d06bdea3e827acb0062726
Matt Parlette <matthew.parlette@gmail.com>
2013-11-12 16:46:50 -0500

    Planets are now landable and scanable by default.
    Closes #31

--- chodewars/planet.py
+++ chodewars/planet.py
@@ -3,6 +3,13 @@ from sector import Sector
 
 class Planet(Entity):
   def __init__(self,initial_state = {}):
+    #Planets should be landable by default (if not already defined)
+    initial_state['landable'] = initial_state['landable'] if 'landable' in initial_state else True
+    
+    #Planets should be scannable by default (if not already defined)
+    initial_state['scanable'] = initial_state['scanable'] if 'scanable' in initial_state else True
+    
+    #Use the Entity __init__ method to initialize this object
     super(Planet,self).__init__(initial_state = initial_state)
     
   def __repr__(self):
